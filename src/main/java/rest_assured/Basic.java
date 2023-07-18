64dd622893b342789f3b41f66a9a1501137061f8
SanjuDeveloper <bhattsanju41@gmail.com>
2023-07-18 10:31:53 -0700

    Initial Commit

---
+++ src/main/java/rest_assured/Basic.java
@@ -0,0 +1,12 @@
+package rest_assured;
+
+import io.restassured.RestAssured.*;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		RestAssured.baseURL="http://127.0.0.1:8000";
+	}
+
+}
