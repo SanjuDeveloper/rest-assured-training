837467a9b7537f51b4e2253c9290abcae357c94e
SanjuDeveloper <bhattsanju41@gmail.com>
2023-07-18 10:39:34 -0700

    staic package added

--- src/main/java/rest_assured/Basic.java
+++ src/main/java/rest_assured/Basic.java
@@ -1,12 +1,14 @@
 package rest_assured;
 
-import io.restassured.RestAssured.*;
+import io.restassured.RestAssured;
+import static  io.restassured.RestAssured.*;
 
 public class Basic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RestAssured.baseURL="http://127.0.0.1:8000";
+		//given().header("content-type","application/json");
 	}
 
 }
