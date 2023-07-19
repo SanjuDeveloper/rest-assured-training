0eddc5ab455cda05138249a312a950a1d4b75a97
SanjuDeveloper <bhattsanju41@gmail.com>
2023-07-19 11:13:53 -0700

    code refector view customer api

--- src/main/java/automation/api/Besic.java
+++ src/main/java/automation/api/Besic.java
@@ -8,8 +8,7 @@ public class Besic {
 	public static void main(String[] args) {
 		RestAssured.baseURI="http://127.0.0.1:8000";
 		given().log().all().when().get("api/viewcustomer")
-		.then().log().all().assertThat().statusCode(200);
-		
+		.then().log().all().assertThat().statusCode(200);		
 		// agr access token send krna h to
 		// given().headers("authorize",tokenValue, "content-type","application/json")
 	}
